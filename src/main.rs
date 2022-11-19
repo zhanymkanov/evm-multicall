b3afc546b6d788fd4e427efbed99ef9a9f4cec47
Yerassyl Zhanymkanov <zhanymkanov@gmail.com>
2022-11-19 22:03:48 +0600

    :recycle: Rename variables

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ use tokens::get_chains_tokens;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let chain_trusted_tokens: HashMap<i64, Vec<TargetToken>> = get_chains_tokens();
+    let chain_tokens: HashMap<i64, Vec<TargetToken>> = get_chains_tokens();
     let address = "0xB25C5E8fA1E53eEb9bE3421C59F6A66B786ED77A";
 
     let mut tasks = FuturesUnordered::new();
     for chain in &chains::CHAINS {
-        let trusted_tokens = chain_trusted_tokens.get(&chain.id).unwrap();
-        tasks.push(chain.get_balances(address, trusted_tokens).boxed());
+        let tokens = chain_tokens.get(&chain.id).unwrap();
+        tasks.push(chain.get_balances(address, tokens).boxed());
     }
     while !tasks.is_empty() {
         tasks.next().await;
